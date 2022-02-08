ad0f8303b731c32b4159eee761f1fc6e67090914
maggness <timderoller@gmail.com>
2022-02-08 14:08:33 +0100

    added drag

--- js/script.js
+++ js/script.js
@@ -1,16 +1,3 @@
-let textLetterT = document.querySelector('#textLetterT');
-let textLetterI = document.querySelector('#textLetterI');
-let textLetterM = document.querySelector('#textLetterM');
-let backgroundKaartjeSection = document.querySelector('#kaartjeSection');
-let infoOnderSection = document.querySelector('#infoOnderaaaa');
-let infoOverTimText = document.querySelector('#infoOverTim');
-let KaartjeSectionbackup1 = document.querySelector('#Kaartjebackup1');
-let KaartjeSectionbackup2 = document.querySelector('#Kaartjebackup2');
-let KaartjeSectionbackup3 = document.querySelector('#Kaartjebackup3');
-let TextVormNummer = 0;
-let textInOverMij = ['Hoi ik ben Tim!', 'Ik ben Grapic Designer & Developer', 'Bekijk mijn website: www.timderoller.nl', 'Email me: Timderoller@gmail.com' , 'Bel me: 0645490344', 'Beweeg me!'];
-let backgroundColor = ['Oranje', 'Red', 'Yellow', 'Green' , 'Blue', 'Paars'];
-
 // getAndRenderData()
 
 // function getAndRenderData () {
@@ -24,39 +11,9 @@ let backgroundColor = ['Oranje', 'Red', 'Yellow', 'Green' , 'Blue', 'Paars'];
 //     }).catch(error => document.body.insertAdjacentHTML('beforebegin', error))
 // }
 
-
-function hoverTextLetterT() {
-
-    let minKans = 0;
-    let maxKans = 5;
-    let TextVormNummer = Math.floor( Math.random() * (maxKans-minKans+1) ) + minKans ;
-
-    let TextVormNummerBackup1 = TextVormNummer + 1;
-    let TextVormNummerBackup2 = TextVormNummer - 1;
-    let TextVormNummerBackup3 = TextVormNummer + 2;
-
-    backgroundKaartjeSection.classList.remove('backgroundOranje', 'backgroundRed', 'backgroundYellow', 'backgroundGreen', 'backgroundBlue', 'backgroundPaars');
-    backgroundKaartjeSection.classList.add('background' + backgroundColor[TextVormNummer]);
-
-    KaartjeSectionbackup1.classList.remove('backgroundOranje', 'backgroundRed', 'backgroundYellow', 'backgroundGreen', 'backgroundBlue', 'backgroundPaars');
-    KaartjeSectionbackup1.classList.add('background' + backgroundColor[TextVormNummerBackup1]);
-
-    KaartjeSectionbackup2.classList.remove('backgroundOranje', 'backgroundRed', 'backgroundYellow', 'backgroundGreen', 'backgroundBlue', 'backgroundPaars');
-    KaartjeSectionbackup2.classList.add('background' + backgroundColor[TextVormNummerBackup2]);
-
-    KaartjeSectionbackup3.classList.remove('backgroundOranje', 'backgroundRed', 'backgroundYellow', 'backgroundGreen', 'backgroundBlue', 'backgroundPaars');
-    KaartjeSectionbackup3.classList.add('background' + backgroundColor[TextVormNummerBackup3]);
-
-    console.log(backgroundColor[TextVormNummer]);
-
-    infoOverTimText.textContent = textInOverMij[TextVormNummer];
-}
-
-function outTextLetterT() {
-    backgroundKaartjeSection.classList.remove('backgroundRed');
-}
-
-dragElement(document.getElementById("backgroundKaartjeSection"));
+dragElement(document.getElementById("backgroundKaartjeTimSection"));
+dragElement(document.getElementById("backgroundKaartjeJodySection"));
+dragElement(document.getElementById("backgroundKaartjeMartijnSection"));
 
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
@@ -98,7 +55,3 @@ function dragElement(elmnt) {
     document.onmousemove = null;
   }
 }
-
-textLetterT.addEventListener("mouseover", hoverTextLetterT);
-textLetterI.addEventListener("mouseover", hoverTextLetterT);
-textLetterM.addEventListener("mouseover", hoverTextLetterT);
